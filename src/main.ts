2fc0cf57189ece02ccfb62a13efe20bc62f6a319
aki <lala.akira@gmail.com>
2020-05-11 21:28:31 +0900

    First Release

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,83 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import Opsworks from 'aws-sdk/clients/opsworks'
+
+interface DeployOptions {
+  region: string
+  StackId: string
+  AppId: string
+}
+
+const deploy = async ({
+  region,
+  StackId,
+  AppId
+}: DeployOptions): Promise<string> => {
+  const opsworks = new Opsworks({region})
+  const {DeploymentId} = await opsworks
+    .createDeployment({
+      Command: {
+        Name: 'deploy',
+        Args: {}
+      },
+      Comment: 'github actions',
+      StackId,
+      AppId
+    })
+    .promise()
+
+  if (!DeploymentId) {
+    throw new Error('Fail deploy')
+  }
+
+  return DeploymentId
+}
+
+interface WaitForOptions {
+  region: string
+  DeploymentId: string
+}
+
+type Status = 'running' | 'successful' | 'failed'
+
+const waitFor = async ({
+  region,
+  DeploymentId
+}: WaitForOptions): Promise<Status> => {
+  const opsworks = new Opsworks({region})
+  const {Deployments} = await opsworks
+    .waitFor('deploymentSuccessful', {
+      DeploymentIds: [DeploymentId]
+    })
+    .promise()
+
+  const deployment = Deployments
+    ? Deployments.find(d => d.DeploymentId === DeploymentId)
+    : null
+
+  if (!deployment || !deployment['Status']) {
+    throw new Error('Deployment not found')
+  }
+
+  return deployment['Status'] as Status
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const region = core.getInput('region', {required: true})
+    const StackId = core.getInput('stackId', {required: true})
+    const AppId = core.getInput('appId', {required: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const DeploymentId = await deploy({region, StackId, AppId})
+    const status = await waitFor({
+      region,
+      DeploymentId
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('status', status)
+
+    if (status !== 'successful') {
+      throw new Error(`Deploy ${status}!`)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
